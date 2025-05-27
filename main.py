0fcc6bec2be0f67281b95cccdae033132851ad38
HarryTurrell <95705572+HarryTurrell@users.noreply.github.com>
2025-05-27 16:32:39 +0100

    attempt at quiz screen
    quiz page there but not working

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
 
 from Screen.Home import HomePage
 
+class QuizScreen(Screen):
+    pass
 
 class HackaburryApp(MDApp):
     def build(self):
         sm = ScreenManager()
         sm.add_widget(HomePage(name="home"))
+        sm.add_widget(QuizScreen(name="quiz"))
         # Add other screens
         return sm
 
